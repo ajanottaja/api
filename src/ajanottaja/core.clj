8dd86917d3ad5aa524f2d1f47c83422b0f603fd5
Snorre Magnus Davøen <post@snorre.io>
2021-10-31 17:55:12 +0100

    Set logging levels at runtime, default to error build time
    Various tooling encounters issues if debug logs are enabled in the
    logback config. To get around this the config now defaults to the error
    log level and only changes the level to info at run time using the
    logback turbo filter functionality.
    
    Signed-off-by: Snorre Magnus Davøen <post@snorre.io>

--- src/ajanottaja/core.clj
+++ src/ajanottaja/core.clj
@@ -1,5 +1,6 @@
 (ns ajanottaja.core
   (:require [cambium.core :as log]
+            [cambium.logback.core.strategy :as strategy]
             [ajanottaja.migrations]
             [ajanottaja.server]
             [ajanottaja.schemas]
@@ -7,6 +8,7 @@
   (:gen-class))
 
 (defn -main [& args]
+  (strategy/set-multi-strategy! "multiStrategy" "info" strategy/multi-forever-validator)
   (log/info "Starting Ajanottaja")
   (.addShutdownHook (Runtime/getRuntime)
                     (Thread. #(mount/stop)))
